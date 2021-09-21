3b0aa64523c7e42260bc9d4d05b9b7c7517fe802
foxcatcher24 <tmitul515@gmail.com>
2021-09-21 13:58:33 +0600

    File & folder structure with production dependencies

---
+++ src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,10 @@
+import PizzaImage from '../../assets/pizza.jpg';
+import styles from './PizzaImage.module.css';
+
+const pizzaImage = ( props ) => (
+    <div className={ styles.PizzaImage }>
+        <img src={ PizzaImage } className={ styles.PizzaImg }/>
+    </div>
+);
+
+export default pizzaImage;
---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component{
+    render(){
+        return(
+            <div>
+                <h1>The Users</h1>
+                <p>Users una</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
